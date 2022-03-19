f1f150098276ccd0bd806cdd5b8eaf6d27bff110
Luis Fructuoso <lfcafn@gmail.com>
2022-03-19 19:08:01 -0400

    Formulario validado con javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+// Validating Form
+const nombre = document.querySelector("#name");
+const email = document.querySelector("#email");
+const mensage = document.querySelector("#message");
+const form = document.querySelector("form");
+const err = document.querySelector(".err");
+
+form.addEventListener("submit", (e) => {
+  let message = [];
+  if (nombre.value === "" || nombre.value === null) {
+    message.push("El nombre es requerido");
+  }
+
+  if (email.value === "" || email.value === null) {
+    message.push("El correo es requerido");
+  }
+
+  if (mensage.value === "" || mensage.value === null) {
+    message.push("El Mensaje es requerido");
+  }
+
+  if (mensage.value.length < 10 && mensage.value.length > 2) {
+    message.push("Seguro tienes algo mas que decir");
+  }
+
+  if (message.length > 0) {
+    e.preventDefault();
+    err.innerText = message.join(", ");
+    err.style.opacity = 1;
+  }
+});
+
 // Footer
 const footerContainer = document.querySelector(".footer__container");
 const fecha = new Date().getFullYear();
